920c3790772a038e4ee984c9eda875a99c699bf8
kenanxasiyevv <kananakh@code.edu.az>
2023-01-03 01:33:59 +0400

    Add project files.

---
+++ EvTapshirigi2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EvTapshirigi2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 15, 25, 35, 55, 78, 90, 12, 74 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 5 == 0 && arr[i] % 7 != 0)
+                    Console.WriteLine(arr[i]);
+
+            }
+        }
+    }
+}
